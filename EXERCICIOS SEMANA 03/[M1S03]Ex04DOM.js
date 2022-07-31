f743ba2e7dc440ebb684a07bdabbb1001d743096
FERNANDO ALEMIDA <fernandopvn2019@gmail.com>
2022-07-31 14:30:28 -0300

    [M1S03]Ex04DOM

---
+++ EXERCICIOS SEMANA 03/[M1S03]Ex04DOM.js
@@ -0,0 +1,11 @@
+function textos(){
+    let t1 = document.getElementById("t1").innerHTML = `Minha energia é o desafio,
+    minha motivação é o impossível,
+    e é por isso que eu preciso
+    ser, à força e a esmo, inabalável.`
+    let t2 = document.getElementById("t2").innerHTML = `Motivação é a arte de fazer as pessoas fazerem o que você quer que elas façam porque elas o querem fazer.`
+    let t3 = document.getElementById("t3").innerHTML = `A verdadeira motivação vem de realização, desenvolvimento pessoal, satisfação no trabalho e reconhecimento.`
+    let t4 = document.getElementById("t4").innerHTML = `Felicidade não é ter o que você quer, é querer o que você tem.`
+    let t5 = document.getElementById("t5").innerHTML = `As pessoas costumam dizer que a motivação não dura sempre. Bem, nem o efeito do banho, por isso recomenda-se diariamente.`
+
+}
