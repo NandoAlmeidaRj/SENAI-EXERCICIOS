8dc4c18b367113d4fac0315da8d6987a4b94625f
FERNANDO ALEMIDA <fernandopvn2019@gmail.com>
2022-08-22 20:23:44 -0300

    M1S8

---
+++ EXERCICIOS SEMANA 08/m1s8/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+export default function App(){
+  return(
+    <divi> 
+      <h1 className='textoum'>Meu primeiro projeto React</h1>
+
+      <p style={{fontStyle:"italic,", fontSize: "50px", textAlign: "center"}}> Criar um projeto React é muito facil </p>
+
+    </divi>
+  )
+}
